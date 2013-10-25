c7074debd2819ad46cb416e78def97e5907103ee
carlos cubillos <ccubillos@humboldt.org.co>
2013-10-25 10:51:24 -0500

    some layout changes to fichas and home- unfinished

---
+++ public/js/app/ui.js
@@ -0,0 +1,20 @@
+// $( document ).ready(function() {
+// 	$( "#recordContent" ).accordion({
+// 		collapsible: true,
+// 		header: "h1"
+// 	});
+// });
+
+
+
+	//$(".collapse").collapse();
+	// $(".collapse").on('show', function () {
+	  
+	//   $("#toggler").removeClass('icon-chevron-left').addClass('icon-chevron-down');
+	// })
+	  
+	// $(".collapse").on('hide', function () {
+	  
+	//   $("#toggler").removeClass('icon-chevron-down').addClass('icon-chevron-left');
+	// })
+
