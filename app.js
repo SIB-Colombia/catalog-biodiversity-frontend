9c58ac533193ef03c5fc6f4acaad0d9c27c5bb96
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2014-02-07 16:09:34 -0500

    Including code for SIB Colombia photblog support

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 /*
  * Module dependencies.
  */
-
 var express = require('express')
   , http = require('http');
 
 var app = express();
+
+var proxy = require("simple-http-proxy"); //Module for SIB Colombia Photoblog proxy, you can delete it without consequences
 
 // Load runtime vars
 require('./config/runtime_vars');
 
 // Load app configuration
 require('./config/environments/all')(app);
+
+app.use("/fotoblog", proxy("http://192.168.16.113:8080/fotoblog/")); //Module for SIB Colombia Photoblog proxy, you can delete it without consequences
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
