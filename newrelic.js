f13f227bff5a77def1f32f5050d8ec00e3f5a3c5
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2014-03-25 11:05:52 -0500

    Including new relic analytics code

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Catálogo de la biodiversidad'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '39892b5e71fea5cd83db5394871314eb4be595ac',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
