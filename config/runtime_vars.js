2e182a3a7a53be9c785fed7c7baf5893aee0a2ec
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2014-09-08 13:50:56 -0500

    Change app name

--- config/runtime_vars.js
+++ config/runtime_vars.js
@@ -4,6 +4,6 @@ appConfigVars = {};
 appConfigVars.backendURL =  'http://www.biodiversidad.co:3000';
 
 // App web server port
-appConfigVars.port = 3000;
+appConfigVars.port = 4000;
 
 module.exports = appConfigVars;
