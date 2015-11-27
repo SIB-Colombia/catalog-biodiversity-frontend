f66473d2666f9137275866dfd8a5ba5e10fc8736
Andres Ciceri <andresciceri@gmail.com>
2015-11-27 16:22:19 -0500

    Views updated and templates

--- config/runtime_vars.js
+++ config/runtime_vars.js
@@ -5,6 +5,6 @@ appConfigVars.backendURL =  'http://www.biodiversidad.co:3000';
 //appConfigVars.backendURL =  'http:/192.168.205.14/catalogo';
 
 // App web server port
-appConfigVars.port = 3000;
+appConfigVars.port = 5002;
 
 module.exports = appConfigVars;
